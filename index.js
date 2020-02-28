e7fd783b585ca93a80a6e9ad4393951534f01f15
Elikem Hermon <eliherm@gmail.com>
2020-02-28 04:01:09 -0500

    Implements docker

--- index.js
+++
@@ -1,8 +0,0 @@
-const chalk = require('chalk'); // Colors terminal text
-const server = require('./api/server');
-
-const port = process.env.PORT;
-
-server.listen(port, () => {
-  console.log(chalk.green(`Server running at http://${process.env.HOST}:${port}`));
-});
